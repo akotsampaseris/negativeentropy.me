56e4eee9960b7e05d2255b961431414b773ab994
akotsampaseris <a.kotsampaseris@gmail.com>
2026-02-26 23:19:41 +0200

    chore: image optimization

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -2,23 +2,16 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-
 const Logo: React.FC = () => {
-    const DEFAULT_LOGO_FILENAME = "logo.svg"
-    const DEFAULT_LOGO_DIR = "/"
-    const DEFAULT_LOGO_PATH = DEFAULT_LOGO_DIR + DEFAULT_LOGO_FILENAME
-    
-    return (
-        <Link href="/">
-            <Image
-            src={DEFAULT_LOGO_PATH}
-            alt="Main Website Logo"
-            width={40}
-            height={50}
-            priority
-            />
-        </Link>
-    )
-}
+    const DEFAULT_LOGO_FILENAME = "logo.webp";
+    const DEFAULT_LOGO_DIR = "/";
+    const DEFAULT_LOGO_PATH = DEFAULT_LOGO_DIR + DEFAULT_LOGO_FILENAME;
 
-export default Logo
+    return (
+        <Link href="/" aria-label="negativeentropy.me home">
+            <Image src={DEFAULT_LOGO_PATH} alt="negativeentropy.me logo" width={40} height={50} priority sizes="40px" />
+        </Link>
+    );
+};
+
+export default Logo;
